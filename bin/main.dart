0e04f2e6ea15b36f294831c352e74f719bb3b6ff
Tomasz Kulinski <t.kajetan.kulinski@gmail.com>
2021-08-01 18:41:57 +0100

    this is very balanced

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,16 @@
 void main() {
   var autoAttack = Ability(10, 15, 'Auto Attack');
-  var human = Race('Human', 0.5, 0.6);
+  var human = Race('Human', 0.8, 0.9);
   var player = Character(human, 'SmallDickChungus', [autoAttack]);
   var vampire = Race(
     'Vampire',
-    0.3,
     0.5,
+    0.7,
   );
   var player2 = Character(vampire, 'BuffiesBitch', [autoAttack]);
   var werewolf = Race('Werewolf', 0.1, 0.6);
   var player3 = Character(werewolf, 'ChadFurry', [autoAttack]);
-  player2.attack(0, player);
+  player3.attack(0, player);
   print(player);
 }
 
