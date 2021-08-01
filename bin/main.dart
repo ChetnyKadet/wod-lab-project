4b3ab903c2a305e58425546d7cd2e2b618a09635
Tomasz Kulinski <t.kajetan.kulinski@gmail.com>
2021-08-01 17:15:33 +0100

    ive killed a human

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,10 @@
 void main() {
-  var player = Character();
+  var human = Race('Human', 0.7);
+  var player = Character(human);
   print(player.toString());
+  player.modifyHealth(-100);
+  print(player.toString());
+
 }
 
 class Character {
@@ -8,6 +12,9 @@ class Character {
   int maxHealth = 200;
   int currentHealth = 100;
   bool isDead = false;
+  final Race race;
+  Character(this.race);
+
   void modifyHealth(int amount) {
     currentHealth += amount;
     if (currentHealth > maxHealth) {
@@ -21,6 +28,18 @@ class Character {
 
   @override
   String toString() {
-    return 'maxHealth $maxHealth\ncurrentHealth $currentHealth\nisDead $isDead';
+    return 'maxHealth $maxHealth\ncurrentHealth $currentHealth\nisDead $isDead\nrace $race';
+  }
+}
+
+class Race {
+  final String raceName;
+  final double damageModifier;
+
+  Race(this.raceName, this.damageModifier);
+
+  @override
+  String toString() {
+    return 'raceName $raceName\ndamageModifier $damageModifier';
   }
 }
