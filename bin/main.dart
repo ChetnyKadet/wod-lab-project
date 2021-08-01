034c85cb5bd3498086f82d8335128ef532369741
Tomasz Kulinski <t.kajetan.kulinski@gmail.com>
2021-08-01 17:22:47 +0100

    now i need options

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,8 @@
 void main() {
   var human = Race('Human', 0.7);
   var player = Character(human);
+  var player = Character(vampire);
+  var player = Character(werewolf);
   var vampire = Race('Vampire', 0.9);
   var werewolf = Race('Werewolf', 1.0);
   print(player.toString());
