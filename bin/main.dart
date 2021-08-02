a7921b4c59ea487f0f5235082be6f6facbe6dbfc
Tomasz Kulinski <t.kajetan.kulinski@gmail.com>
2021-08-02 17:29:12 +0100

    EQUIPMENT

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,13 @@
+import 'ability.dart';
 import 'character.dart';
-
+import 'equipment.dart';
+import 'race.dart';
+import 'stats.dart';
 void main() {
-  var player = Character.humanPlayer('SmallDickChungus');
-  var player2 = Character.vampirePlayer('BuffiesBitch');
-  var player3 = Character.werewolfPlayer('ChadFurry');
+  var player = Character.humanPlayer('SmallDickChungus', );
+  var player2 = Character.vampirePlayer('BuffiesBitch', );
+  var player3 = Character.werewolfPlayer('ChadFurry', );
 
-  player2.attack(0, player3);
+  player3.attack(0, player);
   print(player3);
 }
