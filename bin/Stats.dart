4a4c14c7f64941f3cab9a616cbeaa0197e21cf32
Tomasz Kulinski <t.kajetan.kulinski@gmail.com>
2021-09-08 16:16:08 +0100

    needoo

--- bin/Stats.dart
+++ bin/Stats.dart
@@ -1,5 +1,5 @@
 import 'race.dart';
 
-  const human = Race('Human', 0.8, 0.3, 1, 0);
-  const vampire = Race('Vampire', 0.5, 0.7, 1.5, 0);
-  const werewolf = Race('Werewolf', 0.1, 0.8, 2, 0);
+  const human = Race('Human', 0.8, 0.3, 1, 1);
+  const vampire = Race('Vampire', 0.5, 0.7, 1.5, 1);
+  const werewolf = Race('Werewolf', 0.1, 0.8, 2, 1);
